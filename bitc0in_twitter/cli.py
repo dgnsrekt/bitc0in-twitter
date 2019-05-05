c0ea940652abdaeb2ee4a5e8394442e151200f0c
dgnsrekt <run2devtest@gmail.com>
2019-05-05 09:57:57 -0500

    refactored cli

--- bitc0in_twitter/cli.py
+++ bitc0in_twitter/cli.py
@@ -1,7 +1,4 @@
 import click
-from prompt_toolkit import prompt
-
-keivn = "asfd"
 
 
 @click.group()
@@ -12,24 +9,9 @@ def cli():
 
 
 @cli.command()
-@click.option("--profile", is_flag=True, help="Changes the profiles picture.")
-@click.option("--header", is_flag=True, help="Changes the header.")
-@click.option("--color", is_flag=True, help="Changes the profiles color.")
-@click.option("--desc", is_flag=True, help="Changes the profiles description.")
-@click.option("--suffix", is_flag=True, help="Adds a suffix to the profiles name.")
-@click.option("--all", is_flag=True, help="Changes all profile options.")
-def run(profile, header, color, desc, suffix, all):
+def run():
     """Run Program"""
-    # will run system check on Run
-    # if it cant find env or config file
-    # will tell the user to run configure
     print("RUNNING>>>")
-    if color:
-        print("color")
-    if desc:
-        print("desc")
-    if suffix:
-        print("suff")
 
 
 @cli.command()
