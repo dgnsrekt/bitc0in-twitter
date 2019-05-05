c74dd5631cfd85238e7d2d5bdcb123c12f15c21d
dgnsrekt <run2devtest@gmail.com>
2019-05-05 13:31:50 -0500

    added logging.

--- bitc0in_twitter/btc.py
+++ bitc0in_twitter/btc.py
@@ -1,12 +1,18 @@
 # Provides fuctionality to interact with the bitcoin price.
 
 import ccxt
+import logme
+
+bitcoin_logger = logme.log(scope="module", name="bitcoin")
 
 
 def get_percent_change(currency="BTC/USD"):
+    bitcoin_logger.debug(f"Getting {currency} percentage change.")
     bitmex = ccxt.bitmex()
     response = bitmex.fetch_ticker(currency)
-    return response["percentage"]
+    percent_change = response["percentage"]
+    bitcoin_logger.info(f"{currency} percent change: {percent_change:0.2f} %.")
+    return percent_change
 
 
 if __name__ == "__main__":
