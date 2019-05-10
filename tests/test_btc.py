18b4e8a13d5690cc15b07e50db692abd40a8d6d7
dgnsrekt <run2devtest@gmail.com>
2019-05-09 19:48:32 -0500

    added mark to online test

--- tests/test_btc.py
+++ tests/test_btc.py
@@ -1,6 +1,8 @@
 from bitc0in_twitter.btc import get_percent_change
+import pytest
 
 
+@pytest.mark.online
 def test_get_percent_change():
     btc = get_percent_change()
     assert isinstance(btc, float)
