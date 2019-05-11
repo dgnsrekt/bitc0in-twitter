c0725bbe958b870e89647a8597c1db20a3a0c749
dgnsrekt <run2devtest@gmail.com>
2019-05-11 09:13:05 -0500

    refactored nox to used pipenv.

--- noxfile.py
+++ noxfile.py
@@ -1,30 +1,12 @@
 import nox
-from pathlib import Path
-from io import open
-import json
-
-ROOT_PATH = Path(__file__).parent
-PIPFILE = ROOT_PATH / "Pipfile.lock"
-
-# Parses pipfile.lock
-with open(PIPFILE, encoding="utf-8") as pipfile_lock:
-    pipfile_json = json.loads(pipfile_lock.read())
-    required_packages = pipfile_json["default"]
-    development_packages = pipfile_json["develop"]
-    python_version = pipfile_json["_meta"]["requires"]["python_version"]
-
-REQUIRES = []
-for package, v in required_packages.items():
-    REQUIRES.append(package + v["version"])
 
 
 @nox.session(python="3.7")
 def tests(session):
     """Running tests."""
-    for pack in REQUIRES:
-        session.install(pack)
-    session.install("pytest")
-    session.run("pytest", "--disable-warnings")
+    session.install("pipenv")
+    session.run("pipenv", "install", "--dev")
+    session.run("pipenv", "run", "pytest", "--disable-warnings")
 
 
 @nox.session(python="3.7")
