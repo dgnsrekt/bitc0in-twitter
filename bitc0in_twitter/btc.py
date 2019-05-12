9d89661cbafb1e496805c63a430c0db513daba47
dgnsrekt <run2devtest@gmail.com>
2019-05-11 23:30:32 -0500

    refactored added docstring.

--- bitc0in_twitter/btc.py
+++ bitc0in_twitter/btc.py
@@ -1,16 +1,21 @@
 # Provides fuctionality to interact with the bitcoin price.
 
-import ccxt
+from ccxt import bitmex
 
 import logme
 
 bitcoin_logger = logme.log(scope="module", name="bitcoin")
 
 
-def get_percent_change(currency="BTC/USD"):
+def get_percent_change(currency: str = "BTC/USD") -> float:
+    """Gets the current BTC/USD percent change.
+
+    :param currency: The ticker for the requested currency.
+    :returns: The current 24 hour percent change.
+    """
+
     bitcoin_logger.debug(f"Getting {currency} percentage change.")
-    bitmex = ccxt.bitmex()
-    response = bitmex.fetch_ticker(currency)
+    response = bitmex().fetch_ticker(currency)
     percent_change = response["percentage"]
     bitcoin_logger.info(f"{currency} percent change: {percent_change:0.2f} %.")
     return percent_change
